bb33d9fac35c6d08d4196105ab2df19cab0d27fd
Maksim Kvitko <mukhozuksverdlovsky@gmail.com>
2024-01-16 21:38:48 +0300

    Добавление функционала параметров визита

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,12 +19,61 @@ export class AppComponent {
   }
 
   public submit(buttonType: string): void {
-    console.log("this.form: ", this.form);
+    /**Параметры визита */
+    const metrikaParams = this.getMetrikaParams(buttonType);
 
     // @ts-ignore
-    ym(96113013, "reachGoal", "statements_" + buttonType, undefined, () => {
-      alert("Статистика успешно отправлена");
-    });
+    ym(96113013, "params", metrikaParams);
+
+    alert("Статистика успешно отправлена");
+  }
+
+  /**
+   * Актуально
+   * Метод возвращает актуальные параметры визита
+   */
+  private getMetrikaParams(buttonType: string): IMetrikaParams {
+    let params: IMetrikaParams = {
+      statements_format: this.form.value.format,
+      statements_button: buttonType,
+    };
+
+    const turnovers = [];
+
+    if (this.form.value.turnoverPerDay) {
+      turnovers.push("turnoverPerDay");
+    }
+
+    if (this.form.value.turnoverPerWeek) {
+      turnovers.push("turnoverPerWeek");
+    }
+
+    if (this.form.value.turnoverPerMonth) {
+      turnovers.push("turnoverPerMonth");
+    }
+
+    if (turnovers.length) {
+      params.statements_turnovers = turnovers;
+    }
+
+    return params;
+  }
+
+  /**
+   * Неактуально
+   * Метод отправляет статистику по выполненым целям
+   */
+  private sendMetrikaGoals(buttonType: string): void {
+    // @ts-ignore
+    ym(
+      96113013,
+      "reachGoal",
+      "statements_" + buttonType,
+      { statements_format: this.form.value.format },
+      () => {
+        alert("Статистика успешно отправлена");
+      }
+    );
 
     if (this.form.value.turnoverPerDay) {
       // @ts-ignore
@@ -46,4 +95,10 @@ export class AppComponent {
       ym(96113013, "reachGoal", "statements_format_" + this.form.value.format);
     }
   }
+}
+
+interface IMetrikaParams {
+  statements_format: string;
+  statements_button: string;
+  statements_turnovers?: string[];
 }
